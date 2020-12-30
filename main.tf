c4207468d30885e7af4d14fe849fa292524460bf
Andreas-Perpyrakis <76540815+Andreas-Perpyrakis@users.noreply.github.com>
2020-12-31 00:43:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
   access_key = "${var.aws_access_key}"
@@ -20,4 +11,7 @@ variable "aws_secret_key" {}
 resource "aws_instance" "my_server" {
   ami           = "ami-0885b1f6bd170450c"
   instance_type = "t3.micro"
+    tags = {
+    Name = "My_terraform_Server"
+  }
 }
